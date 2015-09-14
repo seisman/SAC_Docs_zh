c352efbe23081ef8f30d6e0c7f6c806e99f4e3dd
Dongdong Tian <seisman.info@gmail.com>
2015-09-14 22:55:11 +0800

    Python修改发震时刻

---
+++ call-in-script/ch-origin.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+# -*- coding: utf-8 -*-
+import os
+import datetime
+import subprocess
+
+year = 1981
+month = 3
+day = 28
+hour = 10
+min = 23
+sec = 32
+msec = 456
+
+# arguments: year, month, day, hour, minute, second, microsecond
+event = datetime.datetime(year, month, day, hour, min, sec, msec*1000)
+# day of year
+doy = int(event.strftime('%j'))
+
+os.putenv("SAC_DISPLAY_COPYRIGHT", "0")
+
+p = subprocess.Popen(['sac'], stdin=subprocess.PIPE)
+
+s = ""
+s += "r seis.SAC\n"
+s += "ch o gmt %d %d %d %d %d %d\n" % (year, doy, hour, min, sec, msec)
+s += "ch allt (0 - &1,o&) iztype IO\n"
+s += "w over\n"
+s += "quit\n"
+p.communicate(s.encode())
---
+++ call-in-script/rename1.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+foreach my $file (glob "*.SAC") {
+    my ($year, $jday, $hour, $min, $sec, $msec,
+                    $net, $sta, $loc, $chn, $q, $suffix) = split /\./, $file;
+    rename $file, "$net.$sta.$chn";
+}
