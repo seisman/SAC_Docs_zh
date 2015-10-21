5ed2059a77dd9ee2f34648ade04d66dfa5cd2870
Dongdong Tian <seisman.info@gmail.com>
2015-10-21 11:09:06 +0800

    为unwrap命令增加配图

---
+++ scripts/unwrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+export SAC_DISPLAY_COPYRIGHT=0
+sac << EOF
+fg seis
+rmean; rtr; taper
+fft
+wsp origin
+
+fg seis
+rmean; rtr; taper
+unwrap
+wsp unwrap
+r ./origin.ph ./unwrap.ph
+fileid off
+picks off
+p1
+saveimg unwrap.ps
+q
+EOF
+rm origin.am origin.ph unwrap.am unwrap.ph
+ps2raster -A -Tf unwrap.ps
+rm unwrap.ps
