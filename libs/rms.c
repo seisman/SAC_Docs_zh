5f62b625bbca8aea779e6cc72aec3783b5062edd
Dongdong Tian <seisman.info@gmail.com>
2016-01-20 14:26:01 +0800

    libs: rms函数

---
+++ libs/rms.c
@@ -0,0 +1,22 @@
+/*
+ * 本例中使用了Lupei Zhu的sacio库还读入子程序
+ *
+ * 编译方式：
+ *    gcc prog.c sacio.c -lm -L/usr/local/sac/lib -lsac -lsacio
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "sac.h"
+
+double rms(float *x, int nsamps);
+
+int main() {
+    SACHEAD hd;
+    float *data;
+    double result;
+
+    data = read_sac("seis.SAC", &hd);
+    result = rms(data, hd.npts)/sqrt(hd.npts);  // 除以根号N
+    printf("rms = %lf\n", result);
+}
