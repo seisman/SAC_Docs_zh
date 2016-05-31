ef83d88044e06e1dc9a4c802cfbaea38f90eea57
Dongdong Tian <seisman.info@gmail.com>
2016-05-31 13:18:25 +0800

    silent push

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -15,7 +15,7 @@ if [ ${TRAVIS_BRANCH} = "master" -a ${TRAVIS_PULL_REQUEST} = 'false' ]; then
     git config user.email "seisman.info@gmail.com"
     mkdir tmpdir && cp SAC_Docs.pdf tmpdir/
     ghp-import -b pdf tmpdir -m 'Update by travis automatically'
-    git push "https://${GH_TOKEN}@${GH_REF}" pdf:pdf --force
+    git push "https://${GH_TOKEN}@${GH_REF}" pdf:pdf --force --quiet
 else
     echo "Not in master branch"
 fi
