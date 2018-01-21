8065bbb1f6e8cfcb7ac8abae43fb69c049300bcf
Dongdong Tian <seisman.info@gmail.com>
2018-01-22 01:15:53 +0800

    fix travis deploy

--- .travis-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Deploy PDF file to pdf branch
-#
-git config user.name "Dongdong Tian"
-git config user.email "seisman.info@gmail.com"
-mkdir tmpdir && cp SAC_Docs.pdf tmpdir/
-ghp-import -b pdf tmpdir -m 'Update by travis automatically'
-git push "https://${GH_TOKEN}@${GH_REF}" pdf:pdf --force --quiet
