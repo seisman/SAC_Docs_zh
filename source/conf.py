309e94a746a8c8b65226a4707c2d06b77dc3da33
Dongdong Tian <seisman.info@gmail.com>
2018-12-23 21:47:37 -0500

    Bump to 4.1-dev

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,7 @@ master_doc = 'index'
 project = 'SAC参考手册'
 copyright = '2012 - {}, SeisMan'.format(datetime.date.today().year)
 author = 'SeisMan'
-version = '4.0'
+version = '4.1-dev'
 release = version
 language = 'zh_CN'
 
